7945d1e9de3d82fd097f86e84f6ddbe50260543d
Marco Hernandez <marcohern@gmail.com>
2018-03-23 11:58:46 -0500

    building app to laravel

---
+++ laravel5/resources/views/blank.blade.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Bikestickers</title>
+  <base href="/">
+
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" type="image/x-icon" href="favicon.ico">
+</head>
+<body>
+  <app-root></app-root>
+<script type="text/javascript" src="bikestickers/inline.bundle.js"></script>
+<script type="text/javascript" src="bikestickers/polyfills.bundle.js"></script>
+<script type="text/javascript" src="bikestickers/styles.bundle.js"></script>
+<script type="text/javascript" src="bikestickers/vendor.bundle.js"></script>
+<script type="text/javascript" src="bikestickers/main.bundle.js"></script></body>
+</html>
