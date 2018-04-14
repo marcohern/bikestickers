ae2c52aac1bc735aa6dcc139b47ea3abebf2a19d
Marco Hernandez <marcohern@gmail.com>
2018-04-14 17:58:47 -0500

    adding flags, and dropdown

--- angular5/src/app/mods/bike/bike.module.ts
+++ angular5/src/app/mods/bike/bike.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { Page1Component } from './page1/page1.component';
 import { Page2Component } from './page2/page2.component';
 import { Page3Component } from './page3/page3.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    NgbModule
   ],
   declarations: [Page1Component, Page2Component, Page3Component]
 })
