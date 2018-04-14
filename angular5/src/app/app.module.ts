88dfeaced8049d28ccfbac4bc4c2f300ece9b31a
Marco Hernandez <marcohern@gmail.com>
2018-04-14 16:48:22 -0500

    added routing, dropdown works (somewhat) and pages

--- angular5/src/app/app.module.ts
+++ angular5/src/app/app.module.ts
@@ -2,7 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
+import { AppRoutes } from './app.routes';
 import { RequestModule } from './mods/request/request.module';
+import { BikeModule } from './mods/bike/bike.module';
 
 import { AppComponent } from './app.component';
 
@@ -14,7 +16,10 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     NgbModule.forRoot(),
-    RequestModule
+
+    AppRoutes,
+    RequestModule,
+    BikeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
