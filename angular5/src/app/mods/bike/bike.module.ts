034521476f29c0a73fb1bfa697a4f0c46f814772
Marco Hernandez <marcohern@gmail.com>
2018-04-14 19:38:27 -0500

    adding name and next button

--- angular5/src/app/mods/bike/bike.module.ts
+++ angular5/src/app/mods/bike/bike.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { Page1Component } from './page1/page1.component';
 import { Page2Component } from './page2/page2.component';
@@ -8,6 +9,7 @@ import { Page3Component } from './page3/page3.component';
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
     NgbModule
   ],
   declarations: [Page1Component, Page2Component, Page3Component]
--- angular5/src/app/mods/bike/page1/page1.component.ts
+++ angular5/src/app/mods/bike/page1/page1.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-page1',
@@ -7,9 +8,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Page1Component implements OnInit {
 
+  flags:any = [
+    {"selector":"flag-colombia", "name":"colombia"},
+    {"selector":"flag-iran", "name":"iran"},
+    {"selector":"flag-argentina", "name":"argentina"},
+    {"selector":"flag-ukraine", "name":"ukraine"},
+    {"selector":"flag-germany", "name":"germany"},
+    {"selector":"flag-hong-kong", "name":"hong kong"},
+    {"selector":"flag-united-arab-emirates", "name":"united arab emirates"},
+    {"selector":"flag-laos", "name":"laos"},
+    {"selector":"flag-ireland", "name":"ireland"},
+    {"selector":"flag-greece", "name":"greece"},
+    {"selector":"flag-denmark", "name":"denmark"},
+    {"selector":"flag-sweden", "name":"sweden"}
+  ];
+
+  flag:any = null;
+  username:string = '';
+
   constructor() { }
 
   ngOnInit() {
+
   }
 
+  selectFlag(flag:any) {
+    console.log(flag);
+    this.flag = flag;
+  }
+
+  getFlagSelector() {
+    if (this.flag==null) return '';
+    return this.flag.selector;
+  }
+
+  getFlagName() {
+    if (this.flag==null) return '';
+    return this.flag.name;
+  }
+
+  next() {
+    console.log(this.username,this.flag);
+  }
 }
