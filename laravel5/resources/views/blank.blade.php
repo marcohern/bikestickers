0560d453f5891913cf839c34697099dae06e082c
Marco Hernandez <marcohern@gmail.com>
2018-05-02 07:41:04 -0500

    favico, layout

--- laravel5/resources/views/blank.blade.php
+++ laravel5/resources/views/blank.blade.php
@@ -6,7 +6,7 @@
   <base href="/">
 
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="icon" type="image/x-icon" href="favicon.ico">
+  <link rel="icon" type="image/x-icon" href="bikestickers/favicon.ico">
   <link rel="stylesheet" href="bikestickers/assets/bootstrap/css/bootstrap.min.css">
   <link rel="stylesheet" href="bikestickers/assets/flags.css">
   <link rel="stylesheet" href="bikestickers/assets/proride/styles.css">
