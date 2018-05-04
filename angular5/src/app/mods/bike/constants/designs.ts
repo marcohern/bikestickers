9e035e144346319dcf831decb23f6aa883f1d3f4
Marco Hernandez <marcohern@gmail.com>
2018-05-04 16:33:47 -0500

    updated design images

--- angular5/src/app/mods/bike/constants/designs.ts
+++ angular5/src/app/mods/bike/constants/designs.ts
@@ -1,7 +1,7 @@
 import { Design } from "../models/design";
 
 export const designs:Design[] = [
-    {id:1, code:"sticker_bardet"  , name:"Romain Bardet"   , price:0, selected:'btn-outline-dark'},
-    {id:2, code:"sticker_contador", name:"Alberto Contador", price:0, selected:'btn-outline-dark'},
-    {id:3, code:"sticker_nairo"   , name:"Nairo Quintana"  , price:0, selected:'btn-outline-dark'}
+    {id:1, code:"disenos_bardet"  , name:"Romain Bardet" , price:0, selected:'btn-outline-dark'},
+    {id:2, code:"disenos_landa"   , name:"Mikel Landa"   , price:0, selected:'btn-outline-dark'},
+    {id:3, code:"disenos_quintana", name:"Nairo Quintana", price:0, selected:'btn-outline-dark'}
 ];
