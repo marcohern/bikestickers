12c029f877619c629452fec70c51e3df7b2570ae
Marco Hernandez <marcohern@gmail.com>
2018-05-13 00:41:37 -0500

    added contact us form

--- angular5/src/app/mods/bike/contact/contact.component.ts
+++ angular5/src/app/mods/bike/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -7,9 +8,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
+  contactFormGroup:FormGroup = new FormGroup({
+    name: new FormControl(),
+    email: new FormControl(),
+    comments: new FormControl()
+  });
+  sendAlert:boolean = false;
+  sendType:string = '';
+  sendTimeout:any = null;
+  
   constructor() { }
 
   ngOnInit() {
+  }
+
+  showSendAlert() {
+    this.clearSendTimeout();
+    this.sendType = 'success';
+    this.sendAlert = true;
+    this.sendTimeout = setTimeout(() => {
+      this.sendAlert = false;
+      this.clearSendTimeout();
+    }, 5000);
+  }
+
+  showErrorAlert() {
+    this.clearSendTimeout();
+    this.sendType = 'danger';
+    this.sendAlert = true;
+    this.sendTimeout = setTimeout(() => {
+      this.sendAlert = false;
+      this.clearSendTimeout();
+    }, 5000);
+  }
+
+  clearSendTimeout() {
+    if (this.sendTimeout!=null) {
+      clearTimeout(this.sendTimeout);
+      this.sendTimeout = null;
+    }
+  }
+
+  send() {
+    if (this.contactFormGroup.valid) {
+      console.log(this.contactFormGroup.value);
+      this.showSendAlert();
+    } else {
+      this.showErrorAlert();
+    }
   }
 
 }
