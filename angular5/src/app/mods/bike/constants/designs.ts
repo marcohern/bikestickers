f94f4ef4f3278f31716cfbdf0b60c89f7a9dc664
Marco Hernandez <marcohern@gmail.com>
2018-05-12 22:07:17 -0500

    updated design code

--- angular5/src/app/mods/bike/constants/designs.ts
+++ angular5/src/app/mods/bike/constants/designs.ts
@@ -1,7 +1,7 @@
 import { VisualDesign } from "../models/visual-design";
 
 export const designs:VisualDesign[] = [
-    {id:1, code:"disenos_bardet"  ,bg:"sticker_bg_bardet"  ,style:"sticker-bardet"  , name:"Romain Bardet" , price:0, selected:'btn-outline-dark'},
-    {id:2, code:"disenos_landa"   ,bg:"sticker_bg_landa"   ,style:"sticker-landa"   , name:"Mikel Landa"   , price:0, selected:'btn-outline-dark'},
-    {id:3, code:"disenos_quintana",bg:"sticker_bg_quintana",style:"sticker-quintana", name:"Nairo Quintana", price:0, selected:'btn-outline-dark'}
+    {id:1, code:"bardet"  ,bg:"sticker_bg_bardet"  ,style:"sticker-bardet"  , name:"Romain Bardet" , price:0, selected:'btn-outline-dark'},
+    {id:2, code:"landa"   ,bg:"sticker_bg_landa"   ,style:"sticker-landa"   , name:"Mikel Landa"   , price:0, selected:'btn-outline-dark'},
+    {id:3, code:"quintana",bg:"sticker_bg_quintana",style:"sticker-quintana", name:"Nairo Quintana", price:0, selected:'btn-outline-dark'}
 ];
